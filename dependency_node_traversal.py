00cef53ed57a983f09d31570e4f1de7361e29191
huanpc <nauh94@gmail.com>
2019-06-17 23:11:09 +0700

    bi-directional search

--- dependency_node_traversal.py
+++ dependency_node_traversal.py
@@ -1,5 +1,19 @@
+# -*- coding: utf-8 -*-
 """
 A kind of dependency graph traversal with ordering
+cho 1 danh sách N tasks, từ 0->N-1, mỗi task có thể depend vào 1 hay nhiều task khác.nhóm các task này lại thành một list các nhóm sao cho:
+- các task trong một nhóm sẽ không depend lẫn nhau
+- các task trong một nhóm sẽ không depend vào các task ở nhóm kế tiếp
+- số nhóm là nhỏ nhất
+
+testcases:
+
+Input:
+```[[4, 6], [3, 4], [2, 3], [3, 5], [1, 2], [1, 3], [5, 6], [7, 2]]```
+diễn giải: 1 danh sách các cạnh. [4, 6] nghĩa là task số 6 depend vào task số 4
+
+Output:
+```[[1, 7], [2], [3], [4, 5], [6]]```
 """
 import sys
 
@@ -64,6 +78,8 @@ def dependency_node_traversal(edges=[]):
 # Run by: python dependency_node_traversal.py "[[4, 6], [3, 4], [2, 3], [3, 5], [1, 2], [1, 3], [5, 6], [7, 2]]"
 # Your argument is a list of edge V, at each edge, the second element is depent to the first one, 
 # or another words, the first has direct path to the second one.
+# [[4, 6], [3, 4], [2, 3], [3, 5], [1, 2], [1, 3], [5, 6], [7, 2], [4, 1]]
+#
 if __name__ == '__main__':
     edges = eval(sys.argv[1])
     print("Your edge list: " + str(edges))
