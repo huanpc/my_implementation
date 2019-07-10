61fbb9b6e8631d66801509468b51703d9a60054e
huanpc <nauh94@gmail.com>
2019-07-11 00:05:58 +0700

    Add complexity evaluation

--- build_indent_tree.py
+++ build_indent_tree.py
@@ -27,7 +27,9 @@ test = [
 roots = []
 node_to_children = {}
 node_to_name = {}
-
+"""
+complexity: O(n)
+"""
 for i in test:
     if "parentId" in i:
         if i["parentId"] in node_to_children:
@@ -39,12 +41,16 @@ for i in test:
     node_to_name[i['id']] = i['name']
 res = []
 
-
+"""
+complexity: O(n)
+"""
 def trace(space, node):
     print("%s%d(%s)" % (space, node, node_to_name[node]))
     if node in node_to_children:
         for c in node_to_children[node]:
             trace(space + "-", c)
-
+"""
+complexity: O(n)
+"""
 for root in roots:
     trace("-", root)
