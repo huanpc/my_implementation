5d6f5b4d0993bef5e382f15edb704e463af82eaa
huanpc <nauh94@gmail.com>
2019-10-20 14:45:27 +0700

    update problems

--- build_indent_tree.py
+++ build_indent_tree.py
@@ -38,33 +38,52 @@ output:
 
 ##
 roots = []
-node_to_children = {}
-node_to_name = {}
-"""
-complexity: O(n)
-"""
+id_to_node = {}
 for i in test:
+    if i['id'] not in id_to_node:
+        id_to_node[i['id']] = []
+
     if "parentId" in i:
-        if i["parentId"] in node_to_children:
-            node_to_children[i["parentId"]].append(i["id"])
+        if i["parentId"] in id_to_node:
+            id_to_node[i["parentId"]].append(id_to_node[i['id']])
         else:
-            node_to_children[i["parentId"]] = [i["id"]]
+            id_to_node[i["parentId"]] = [id_to_node[i['id']]]
+
     else:
         roots.append(i["id"])
-    node_to_name[i['id']] = i['name']
-res = []
+        if i['id'] not in id_to_node:
+            id_to_node[i['id']] = []
+print(id_to_node)
 
-"""
-complexity: O(n)
-"""
-def trace(space, node):
-    print("%s%d(%s)" % (space, node, node_to_name[node]))
-    if node in node_to_children:
-        for c in node_to_children[node]:
-            trace(space + "-", c)
-"""
-complexity: O(n)
-"""
-for root in roots:
-    trace("-", root)
-
+# ##
+# roots = []
+# node_to_children = {}
+# node_to_name = {}
+# """
+# complexity: O(n)
+# """
+# for i in test:
+#     if "parentId" in i:
+#         if i["parentId"] in node_to_children:
+#             node_to_children[i["parentId"]].append(i["id"])
+#         else:
+#             node_to_children[i["parentId"]] = [i["id"]]
+#     else:
+#         roots.append(i["id"])
+#     node_to_name[i['id']] = i['name']
+# res = []
+#
+# """
+# complexity: O(n)
+# """
+# def trace(space, node):
+#     print("%s%d(%s)" % (space, node, node_to_name[node]))
+#     if node in node_to_children:
+#         for c in node_to_children[node]:
+#             trace(space + "-", c)
+# """
+# complexity: O(n)
+# """
+# for root in roots:
+#     trace("-", root)
+#
