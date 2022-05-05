1ccbe72330b434a938a22cdc7ea7db5d6ec8dd5c
Huan <huan.phancong@shopee.com>
2022-05-05 23:28:37 +0800

    restructure

--- skip_list_implementation.py
+++
@@ -1,8 +0,0 @@
-# Implementation of skiplist (https://en.wikipedia.org/wiki/Skip_list)
-
-
-class Node:
-
-    def __init__(self, key, level):
-        self.key = key
-        self.next = [None] * (level + 1)
